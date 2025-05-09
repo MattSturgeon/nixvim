dbccec8020d693e9745b09f4ba4797b6026439c0
Matt Sturgeon <matt@sturgeon.me.uk>
2025-05-09 08:56:49 +0100

    modules/plugins: declare docs pages

--- modules/plugins.nix
+++ modules/plugins.nix
@@ -1,7 +1,7 @@
-{ lib, ... }:
+{ lib, options, ... }:
 let
-  inherit (builtins) readDir;
-  inherit (lib.attrsets) foldlAttrs;
+  inherit (builtins) readDir mapAttrs;
+  inherit (lib.attrsets) genAttrs filterAttrs foldlAttrs;
   inherit (lib.lists) optional;
   by-name = ../plugins/by-name;
 in
@@ -12,4 +12,21 @@ in
       prev: name: type:
       prev ++ optional (type == "directory") (by-name + "/${name}")
     ) [ ] (readDir by-name);
+
+  # Declare plugins and colorschemes pages
+  config.meta.pages = genAttrs [ "plugins" "colorschemes" ] (set: {
+    optionPredicate = opt: lib.lists.hasPrefix [ set ] opt.loc;
+
+    # Declare separate sub-pages for each plugin
+    pages = lib.pipe options.${set} [
+      (filterAttrs (_: opt: !lib.isOption opt))
+      (mapAttrs (
+        name: _: {
+          optionPredicate = opt: lib.lists.hasPrefix [ set name ] opt.loc;
+          # TODO: sub-pages for settings, etc?
+          # TODO: description, etc
+        }
+      ))
+    ];
+  });
 }
