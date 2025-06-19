d0d4ef74eb128480e684494bedd7c0ebc9d24b6a
Heitor Augusto <IAm.HeitorALN@proton.me>
2025-06-19 19:51:06 -0300

    plugins/otter: add `autoActivate` support for the new `lsp` module

--- plugins/by-name/otter/default.nix
+++ plugins/by-name/otter/default.nix
@@ -114,6 +114,10 @@ lib.nixvim.plugins.mkNeovimPlugin {
     plugins.lsp.onAttach = lib.mkIf cfg.autoActivate ''
       require('otter').activate()
     '';
+
+    lsp.onAttach = lib.mkIf cfg.autoActivate ''
+      require('otter').activate()
+    '';
   };
 
 }
